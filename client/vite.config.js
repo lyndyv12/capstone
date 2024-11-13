6c7288b34a6376db34ebf2637d6c0aad34cd9ac5
Lyndsay Vance <lyndsayvance@Lyndsays-MacBook-Pro.local>
2024-11-13 07:24:41 -0800

    config changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': `http://localhost:${ serverPort || 3000}`
+      '/api': `https://lvreviews.onrender.com/`
     }
   },
   build: {
