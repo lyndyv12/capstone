89889f386fc4b567e53a9782cedc971e8a33f0be
Lyndsay Vance <lyndsayvance@Lyndsays-MacBook-Pro.local>
2024-11-13 07:50:10 -0800

    config changes for backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const serverPort = process.env.PORT || 3000;
+const backendUrl = process.env.VITE_BACKEND_URL || 'http://localhost:3000'; 
+
+
 console.log(`api need to run on ${serverPort} for vite server`);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': `https://lvreviews.onrender.com/`
+      '/api': backendUrl,
     }
   },
   build: {
